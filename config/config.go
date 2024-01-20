8334bb5ac3a44da71a7ae10610150393ae1d10e8
xbytee <nameerrror3301@gmail.com>
2024-01-20 18:42:10 -0500

    INIT: Реализовал конфиг для приложения и логгер

--- config/config.go
+++ config/config.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+)
+
+type Config struct {
+	App App `yaml:"app"`
+}
+
+type App struct {
+	Mode   string `yaml:"mode"`
+	Logger Logger `yaml:"logger"`
+	Grpc   Grpc   `yaml:"grpc"`
+}
+
+type Logger struct {
+	Mode string `yaml:"mode"`
+}
+
+type Grpc struct {
+	Port    int `yaml:"port"`
+	Timeout int `yaml:"timeout"`
+}
+
+func ReadConfig(path string) Config {
+	if _, err := os.Stat(path); err != nil {
+		panic("Err not exist config.yaml file: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("Err read config file: " + err.Error())
+	}
+
+	return cfg
+}
---
+++ pkg/logger/interface.go
@@ -0,0 +1,9 @@
+package logger
+
+type Interface interface {
+	Debug(message interface{}, args ...interface{})
+	Info(message string, args ...interface{})
+	Warn(message string, args ...interface{})
+	Error(message interface{}, args ...interface{})
+	Fatal(message interface{}, args ...interface{})
+}
